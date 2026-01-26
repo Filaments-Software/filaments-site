1be8c22d0c9030f79db4c624f07bb0bfe897a46d
Arcev <29128256+Arcevalis@users.noreply.github.com>
2026-01-25 18:24:10 -0800

    Change Arcev's bio

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -164,7 +164,7 @@ export default function AboutPage() {
                   name: "Arcev",
                   role: "Founder",
                   image: "/images/team-1.png",
-                  bio: "TODO: Add a ChatGPT prompt here",
+                  bio: "How it be, what it do?",
                   style: "contain",
                 },
                 {
